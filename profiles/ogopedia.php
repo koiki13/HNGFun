d235bb954971b8db5937090b56b631ffa5cf5e35
Travis CI <travis@travis-ci.org>
2018-05-08 21:19:43 +0000

    committed at 2ff288f [ci skip]

--- profiles/ogopedia.php
+++ profiles/ogopedia.php
@@ -82,7 +82,6 @@ img.ogo {
 
 .apa-otun {
     float: right;
-    /*padding-top: 100px;*/
     padding-top: 20%;
 }
 
@@ -94,8 +93,6 @@ img.social {
     width: 50px;
     padding-bottom: 20px;
 }
-
-
 
 .apa-osi {
     width: 65%;
@@ -119,9 +116,8 @@ p {
     font-weight: 100;
     line-height: normal;
     margin: 0;
-    letter-spacing: 1.4px;
+    letter-spacing: 1px;
 }
-
 
 @media(max-width: 999px) {
     .apa-osi {
@@ -200,7 +196,7 @@ p {
 
 .bg-primary {
     background: transparent !important;
-    border-bottom: 1px solid rgba(233, 234, 238, 0.1);
+    border-bottom: 1px solid rgba(233, 234, 238, 0.1) !important;
 }
 
 footer {
